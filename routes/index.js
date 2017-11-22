a77fcac0390d10fbd9e10264fc1c935ae8039ba6
bfelinskyi <felinskyi@gmail.com>
2017-11-22 19:36:01 +0200

    directory refactor

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,47 @@ module.exports = function (app, addon) {
         });
     });
 
+    // This is an example route that's used by the default "generalPage" module.
+    // Verify that the incoming request is authenticated with Atlassian Connect
+    app.get('/hello-world', addon.authenticate(), function (req, res) {
+            // Rendering a template is easy; the `render()` method takes two params: name of template
+            // and a json object to pass the context in
+            res.render('hello-world', {
+                title: 'Atlassian Connect'
+                //issueId: req.query['issueId']
+            });
+        }
+    );
+
+    // Add any additional route handlers you need for views or REST resources here...
     app.get('/dropdown', function(req,res){
         res.render("dropdown", {id : req.query['id'], mode : req.query['mode'] });
     });
 
+    app.get('/activity', addon.authenticate(), function(req, res) {
+        res.render('activity', { title: "JIRA activity" });
+    });
+
+    app.get('/installed', addon.authenticate(), function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/event-stream'});
+    });
+
+    // load any additional files you have in routes and apply those to the app
+    {
+        var fs = require('fs');
+        var path = require('path');
+        var files = fs.readdirSync("routes");
+        for(var index in files) {
+            var file = files[index];
+            if (file === "index.js") continue;
+            // skip non-javascript files
+            if (path.extname(file) != ".js") continue;
+
+            var routes = require("./" + path.basename(file));
+
+            if (typeof routes === "function") {
+                routes(app, addon);
+            }
+        }
+    }
 };
